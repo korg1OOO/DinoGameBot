83be40da7d4b33d4ee153986e703e6da0f6a296a
korg1OOO <118229774+korg1OOO@users.noreply.github.com>
2023-07-29 15:44:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ import time
 from PIL import ImageGrab
 import pyautogui
 
-# bg_color = (247, 247, 247)
-# dino_color = (83, 83, 83)
+X = 655
 def capture_screen():
-    screen = imageGrab.grab()
+    screen = ImageGrab.grab()
     return screen
 
 def detect_enemy(screen):
-    for x in range (600, 650):
-        for y in range(240, 250):
+    aux_color = screen.getpixel((int(X), 215))
+    for x in range (int(X), int(X+15)):
+        for y in range(215, 250):
             color = screen.getpixel((x,y))
-            if color = (83, 83, 83):
+            if color != aux_color:
                 return True
+            else:
+                aux_color = color
 
 def jump():
-    py
+    global X
+    pyautogui.press("up")
+    X += 0.4
 
 print("Start in 3 seconds...")
 time.sleep(3)
 
 while True:
     screen = capture_screen()
-    if detect_enemy():
+    if detect_enemy(screen):
         jump()
